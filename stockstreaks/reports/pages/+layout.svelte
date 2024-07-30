96dcf414d04bb2e5305a980c0a4fb7747e50c2b6
Chris Davis <44245221+thedatadavis@users.noreply.github.com>
2024-07-30 19:19:42 -0400

    Setting up evidence project

---
+++ stockstreaks/reports/pages/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import '@evidence-dev/tailwind/fonts.css';
+	import '../app.css';
+	import { EvidenceDefaultLayout } from '@evidence-dev/core-components';
+	export let data;
+</script>
+
+<EvidenceDefaultLayout {data} 
+    hideSidebar={true} 
+    hideHeader={true}
+    hideTOC={true}
+    hideBreadcrumbs={true}
+>
+	<slot slot="content" />
+</EvidenceDefaultLayout>
---
+++ stockstreaks/reports/pages/tickers/[ticker].md
@@ -0,0 +1,3 @@
+---
+title: A Stock Page
+---
